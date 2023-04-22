52323605a9522b19cb06b0139bd48f59fe336a4f
Shamanesss <mentulbenodorna@gmail.com>
2023-04-22 10:23:07 +0200

    deploy

--- src/views/ComponenteView.vue
+++ src/views/ComponenteView.vue
@@ -1,20 +1,18 @@
-
-  <template>
-    <div class="example">
-       
-       <h1>Este es un nuemo componente BOTON</h1> 
-       <componente-example></componente-example>
-       <componente-example></componente-example>
-    </div>
-  </template>
+<template>
+  <div class="example">
+    <h1>Este es un nuevo componente BOTON</h1>
+    <componente-example></componente-example>
+    <componente-example></componente-example>
+  </div>
+</template>
 <script>
-import ComponenteExample from '@/components/ComponentExample.vue';
+import ComponenteExample from "@/components/ComponentExample.vue";
 
 export default {
-    name: "ComponenteView",
-    props: {
-        example: String
-    },
-    components: { ComponenteExample }
-}
+  name: "ComponenteView",
+  props: {
+    example: String,
+  },
+  components: { ComponenteExample },
+};
 </script>
