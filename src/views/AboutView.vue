7bd7a590b8eababc9f666f0e874f135886783135
Shamanesss <mentulbenodorna@gmail.com>
2023-04-22 10:31:34 +0200

    agregando dist subtree

--- src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-  
-  
-    <h1>Un dia cualquiera</h1>
-    <img src="@/assets/caballero.gif" class="rounded mx-auto d-block" alt="developer">
-  </div>
-</template>
